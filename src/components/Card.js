0e9ea352e9c04e31fee5ad1632661e4583108b6b
Templo Sion Tijuana@ <templosiontijuana@gmail.com>
2021-01-11 22:23:45 -0800

    commit styles on line

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,29 @@
 import React from 'react'
 import ExerciseImg from '../images/exercise.png'
+import CirclesImg from '../images/circles.png'
+import './styles/Card.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
 class Card extends React.Component{
     render(){
         return (
-            <div>
-                <div>
-                    <img src={ExerciseImg}/>
-                </div>
-                <div>
-                    <h1>Guia de tecnicas</h1>
-                    <p>Encuentra una seria de tecnias en esta guia aqui 2</p>
-                </div>
-            </div>
+           <div className="card mx-auto Fitness-Card"
+                style={{
+                    backgroundImage: `url(${CirclesImg}), linear-gradient(to right, #A74CF2, #617BFB)`
+                }}
+           >
+               <div className="card-body">
+                    <div className="row center">
+                        <div className="col-6">
+                            <img src={ExerciseImg} className="float-right"/>
+                        </div>
+                        <div className="col-6 Fitness-Card-Info">
+                            <h1>Guia de tecnicas</h1>
+                            <p>Encuentra una seria de tecnias en esta guia</p>
+                        </div>
+                    </div>
+               </div>
+           </div>
         )
     }
 }
